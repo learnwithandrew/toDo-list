004962e4d909f83962082fe7b297dba17f3e6675
andyjohn23 <andrewnick906@gmail.com>
2024-05-22 02:44:28 +0300

    create task post form

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let taskContainer = document.querySelector('.toDo-list')
+    let taskContainer = document.querySelector('.toDo-list');
+    let taskSubmitForm = document.getElementById('taskForm');
+    let taskTitle = document.getElementById('taskTitle');
+
     let displayTasks = () => {
         fetch('http://localhost:3000/tasks')
             .then(response => response.json())
@@ -9,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     let taskItem = document.createElement('div')
                     taskItem.classList.add('toDo-item')
                     taskItem.innerHTML = `
-                    <h1>${task.taskName}</h1>
+                    <h1>${task.taskName.toUpperCase()}</h1>
                     <div class="modify-buttons">
                         <i class="fa fa-pencil-square" aria-hidden="true"></i>
                         <i class="fa fa-trash" aria-hidden="true"></i>
@@ -18,8 +21,25 @@ document.addEventListener('DOMContentLoaded', () => {
                     taskContainer.appendChild(taskItem)
                 })
             })
-
     }
+
+    let taskCreateForm = (e) => {
+        e.preventDefault();
+
+        const taskName = taskTitle.value;
+        const taskItem = {
+            taskName
+        }
+        fetch('http://localhost:3000/tasks', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(taskItem)
+        }).then(response => response.json()).then((data) => { console.log(data) })
+    }
+
+    taskSubmitForm.addEventListener('submit', taskCreateForm);
     displayTasks();
 })
 
